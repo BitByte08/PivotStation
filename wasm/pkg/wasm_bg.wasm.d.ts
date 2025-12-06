66c62ba65808a8fb18b4a2bbe5142f60a136b26d
bitbyte08 <me@bitworkspace.kr>
2025-12-06 21:49:38 +0900

    fix :: 빌드 결과 추가

---
+++ wasm/pkg/wasm_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const interpolate_frame: (a: any, b: any, c: number) => any;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_start: () => void;
