b655dac0716950e88f5cc3cab712f717f03b47b1
bitbyte08 <me@bitworkspace.kr>
2025-12-07 00:44:07 +0900

    fix :: 라우팅 에러 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 dark:bg-black p-24">
@@ -8,8 +6,8 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left gap-4">
-        <Link
-          href="/editor"
+        <a
+          href="/editor/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
           <h2 className={`mb-3 text-2xl font-semibold text-black dark:text-white`}>
@@ -21,7 +19,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-black dark:text-white`}>
             Start a new animation project from scratch.
           </p>
-        </Link>
+        </a>
       </div>
     </div>
   );
